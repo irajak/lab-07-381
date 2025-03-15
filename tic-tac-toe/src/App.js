12c9e249610132deb71d86d0d27193dd75552b81
alzeirjaf <irajakbar03@gmail.com>
2025-03-14 21:00:36 -0600

    add data to props

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function Square() {
+function Square({ value }) {
   return (
     <button className="square">
-      1
+      {value}
     </button>
   );
 }
@@ -13,19 +13,19 @@ function Board() {
   return (
     <div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </div>
   );
