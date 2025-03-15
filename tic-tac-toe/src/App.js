6c79e64821bb2d143e3db5fa18eff20d7632e618
alzeirjaf <irajakbar03@gmail.com>
2025-03-14 21:06:37 -0600

    i think final?

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -48,9 +48,9 @@ function Board() {
 
 function calculateWinner(squares) {
   const lines = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-    [0, 4, 8], [2, 4, 6], // Diagonals
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],  // row
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],  // column
+    [0, 4, 8], [2, 4, 6],             // diagonal
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
