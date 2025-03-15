2fca9b7365caa9a66b66446a477dedb65544c0a7
alzeirjaf <irajakbar03@gmail.com>
2025-03-14 21:01:53 -0600

    turns

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -11,11 +11,14 @@ function Square({ value, onSquareClick }) {
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
+    if (squares[i]) return;
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    nextSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
